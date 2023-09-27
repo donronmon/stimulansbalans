b72389ea2bc0aa0eebb2f0ee62773f51bce968e7
Ron <ron@unalu.me>
2023-09-27 17:44:44 +0200

    astro update, cloudflare pages added...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
-    experimental: { assets: true },
-  });
+  output: "server",
+  adapter: cloudflare()
+});
